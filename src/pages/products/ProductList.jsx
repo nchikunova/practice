25fde452deaf3c1646a3eb99970d7320c51db678
Nataliya <nchikunova86@gmail.com>
2021-12-29 15:44:30 +0200

    added del func for product and corrected func for required fields

--- src/pages/products/ProductList.jsx
+++ src/pages/products/ProductList.jsx
@@ -1,15 +1,15 @@
-import ProductItem from "./ProductItem";
+import ProductItem from './ProductItem';
 
-const Productlist = ({products}) => {
-   return (
-      <ul>
-         {products.map((product) => product.count ? (
-            <ProductItem key={product.id} product={product}/>
-         ) : null,
-         )}
-      </ul>
-   );
-}
+const Productlist = ({ products, onDelete }) => {
+  return (
+    <ul>
+      {products.map(product =>
+        product.count ? (
+          <ProductItem key={product.id} product={product} onDelete={onDelete} />
+        ) : null,
+      )}
+    </ul>
+  );
+};
 
 export default Productlist;
-
